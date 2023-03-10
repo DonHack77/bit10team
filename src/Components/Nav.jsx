83ec4386aa8786841d66c2fc8b9261ebceedc7fc
UhaneKaumaha <melanyalva01@gmail.com>
2023-03-09 23:53:28 -0500

    fix nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,20 +8,38 @@ import { Suggestions } from "../Pages/Suggestions";
 export const Nav = () => {
   return (
     <BrowserRouter >
-      <nav className="py-4 text-center">
-        <Link className="mx-2" to="/">
-          Pokedex
-        </Link>
-        <Link className="mx-2" to="/Historia">
-          Historia
-        </Link>
-        <Link className="mx-2" to="/Equipo">
-          Equipo
-        </Link>
-        <Link className="mx-2" to="/Sugerencias">
-          Sugerencias
-        </Link>
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-danger">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
+            aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav ms-auto me-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Pokedex
+                  <span className="visually-hidden">(current)</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Historia">Historia</Link>
+              </li>
+                <img className="img-pokeball" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/64px-Pok%C3%A9_Ball_icon.svg.png" alt="" />
+              <li className="nav-item">
+                <Link className="nav-link" to="/Equipo">Equipo</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/Sugerencias">Sugerencias</Link>
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
       </nav>
+
+
 
       <Routes>
         <Route path="/" element={<Index />}></Route>
