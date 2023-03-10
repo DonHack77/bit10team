73f6539425d8e69066b59b38d4375ae23b062824
UhaneKaumaha <melanyalva01@gmail.com>
2023-03-10 15:00:48 -0500

    fix logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Nav } from './Components/Nav';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap";
 
 function App() {
   return(
