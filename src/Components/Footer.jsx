31b176dbcb33e69f0b3d1c5a2e1574e521644f64
UhaneKaumaha <melanyalva01@gmail.com>
2023-03-10 11:53:48 -0500

    abrir en nueva pestaña

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,13 +11,13 @@ export const Footer = () => {
 
 
           <section className="mb-1">
-            <a className="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/Pokemon/?locale=es_LA" role="button">
+            <a className="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/Pokemon/?locale=es_LA" target="_blank"role="button">
               <FontAwesomeIcon icon={faFacebookF} />
             </a>
-            <a className="btn btn-outline-light btn-floating m-1" href="https://twitter.com/Pokemon?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" role="button">
+            <a className="btn btn-outline-light btn-floating m-1" href="https://twitter.com/Pokemon?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank" role="button">
               <FontAwesomeIcon icon={faTwitter} />
             </a>
-            <a className="btn btn-outline-light btn-floating m-1" href="https://www.pokemon.com/us" role="button">
+            <a className="btn btn-outline-light btn-floating m-1" href="https://www.pokemon.com/us" target="_blank" role="button">
               <FontAwesomeIcon icon={faGoogle} />
             </a>
             <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
